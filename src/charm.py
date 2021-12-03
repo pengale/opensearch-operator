e47b8d2151db8ae21fc24e61b76b8f59d8e694f4
Pete Vander Giessen <petevg@canonical.com>
2021-12-03 13:25:13 -0500

    Sketched out some basics.
    This is just laying out ideas in the appropriate places in the
    charm. Nothing fancy yet!

--- src/charm.py
+++ src/charm.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
-# Copyright 2021 Penny Gale
+# Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
-#
-# Learn more at: https://juju.is/docs/sdk
 
-"""Charm the service.
-
-Refer to the following post for a quick-start guide that will help you
-develop a new k8s charm using the Operator Framework:
-
-    https://discourse.charmhub.io/t/4208
-"""
+"""Charmed operator for OpenSearch, an open source search and analytics engine."""
 
 import logging
 
+from charms.observability_libs.v0.kubernetes_service_patch import KubernetesServicePatch
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
@@ -22,19 +15,22 @@ from ops.model import ActiveStatus
 logger = logging.getLogger(__name__)
 
 
-class CharmOpensearchCharm(CharmBase):
-    """Charm the service."""
+class OpensearchCharm(CharmBase):
+    """Charmed operator for OpenSearch, an open source search and analytics engine."""
 
     _stored = StoredState()
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.httpbin_pebble_ready, self._on_httpbin_pebble_ready)
+        self.framework.observe(self.on.opensearch_pebble_ready, self._on_opensearch_pebble_ready)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.fortune_action, self._on_fortune_action)
-        self._stored.set_default(things=[])
+        self.framework.observe(self.on.inject_action, self._on_inject_action)
+        self.framework.observe(self.on.inject_action, self._on_inject_action)
+        self.framework.observe(self.on.cred_action, self._on_creds_action)
 
-    def _on_httpbin_pebble_ready(self, event):
+        self._service_patcher = KubernetesServicePatch(self, [(self.app.name, 4080, 4080)])
+
+    def _on_opensearch_pebble_ready(self, event):
         """Define and start a workload using the Pebble API.
 
         TEMPLATE-TODO: change this example to suit your needs.
@@ -46,22 +42,28 @@ class CharmOpensearchCharm(CharmBase):
         """
         # Get a reference the container attribute on the PebbleReadyEvent
         container = event.workload
+
+        # TODO: fetch or generate creds
+        
         # Define an initial Pebble layer configuration
         pebble_layer = {
-            "summary": "httpbin layer",
-            "description": "pebble config layer for httpbin",
+            "summary": "opensearch layer",
+            "description": "pebble config layer for opensearch",
             "services": {
-                "httpbin": {
+                "opensearch": {
                     "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
+                    "summary": "opensearch",
+                    # TODO: build args out of the config.
+                    "command": "./opensearch-entrypoint.sh"
                     "startup": "enabled",
-                    "environment": {"thing": self.model.config["thing"]},
+                    "environment": {
+                        # TODO: add admin_user, admin_password, and other info.
+                    },
                 }
             },
         }
         # Add intial Pebble config layer using the Pebble API
-        container.add_layer("httpbin", pebble_layer, combine=True)
+        container.add_layer("opensearch", pebble_layer, combine=True)
         # Autostart any services that were defined with startup: enabled
         container.autostart()
         # Learn more about statuses in the SDK docs:
@@ -69,35 +71,31 @@ class CharmOpensearchCharm(CharmBase):
         self.unit.status = ActiveStatus()
 
     def _on_config_changed(self, _):
-        """Just an example to show how to deal with changed configuration.
+        # TODO: config for dashboard, data set, etc.
+        pass
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle config, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the config.py file.
+    def _on_inject_action(self, event):
+        """Injest a given dataset.
 
-        Learn more about config at https://juju.is/docs/sdk/config
-        """
-        current = self.config["thing"]
-        if current not in self._stored.things:
-            logger.debug("found a new thing: %r", current)
-            self._stored.things.append(current)
-
-    def _on_fortune_action(self, event):
-        """Just an example to show how to receive actions.
-
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle actions, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the actions.py file.
-
-        Learn more about actions at https://juju.is/docs/sdk/actions
+        TODO: what params to pass?
         """
         fail = event.params["fail"]
         if fail:
             event.fail(fail)
         else:
-            event.set_results({"fortune": "A bug in the code is worth two in the documentation."})
+            event.set_results({"injest": "Not yet implemented."})
+
+    def _on_creds_action(self, event):
+        """Return the admin creds."""
+        username = "not yet implemeneted"
+        password = "not yet implemented"
+
+
+        fail = event.params["fail"]
+        if fail:
+            event.fail(fail)
+        else:
+            event.set_results({"username": username, "password": password})
 
 
 if __name__ == "__main__":
